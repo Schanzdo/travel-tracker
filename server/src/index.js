01056192020c037078607be111a91d5f76a104e2
SchanzDo <schanz.dominik@gmx.de>
2020-08-30 10:42:48 +0200

    added error handling

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,31 @@
-const express = requre('express')
+const express = require('express')
+const morgan = require('morgan') //logger
+const helmet = require('helmet')// hide header infos 
+const cors = require('cors')
+
+const errorHandling = require('./errorHandling');
 
 const app =  express();
+app.use(morgan('common'));
+app.use(helmet())
+app.use(cors({
+    origin: 'https://localhost:3000'
+}));
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the Travel tracker Backend server'
+    })
+})
+
+
+
+app.use(errorHandling.notFound);
+app.use(errorHandling.errorHandler); 
+
 const port = process.env.PORT || 1337;
 
 app.listen(port, ()=> {
-    console.log("Goto http://localhost:$(port)");
+    console.log(`Goto http://localhost:${port}`);
 })
